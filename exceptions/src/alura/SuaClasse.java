6b17c9cffbc3916efcc54282f1a39f7be09b1c19
Alexandre Nobrega <alexandrenobrega777@gmail.com>
2023-02-23 08:32:37 -0300

    [Exceptions] - Alura - Exemplos Tratamento de Exceções

---
+++ exceptions/src/alura/SuaClasse.java
@@ -0,0 +1,12 @@
+package alura;
+
+public class SuaClasse {
+
+    public void fazAlgo(int[] idades) {
+        System.out.println(idades[1]);
+    }
+
+    public static void main(String[] args) {
+        new SuaClasse().fazAlgo(new int[0]);
+    }
+}
---
+++ exceptions/src/alura/TesteTrataException.java
@@ -0,0 +1,30 @@
+package alura;
+
+import java.io.IOException;
+
+public class TesteTrataException {
+
+    public static void main(String[] args) {
+        String nome = null;
+
+        //Exemplo com exceção não verificada
+        try {
+            nome.toLowerCase();
+            System.out.println("Essa mensagem não será chamada/impressa, pois ocorre uma exceção e a execução já vai direto para o catch");
+        } catch(NullPointerException e) {
+            System.out.println("Peguei a exception não verificada");
+        }
+            System.out.println("Terminei");
+
+            System.out.println();
+
+        //Exemplo com exceção verificada
+        try {
+            new java.io.FileInputStream("a.txt");
+            System.out.println("Essa mensagem não será chamada/impressa, pois ocorre uma exceção e a execução já vai direto para o catch");
+        } catch(IOException e) {
+            System.out.println("Peguei a exception verificada");
+        }
+            System.out.println("Terminei");
+    }
+}
