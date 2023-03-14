5f5064d3e1bc0bf63a76a37d41229c8aa30d8ab4
Alexandre Nobrega <alexandrenobrega777@gmail.com>
2023-03-14 11:44:14 -0300

    [Exceptions] - criando classe Main para testar exceção verificada FileNotFoundException

---
+++ exceptions/src/alura/Main.java
@@ -0,0 +1,13 @@
+package alura;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        AcessoAoArquivo aa = new AcessoAoArquivo();
+
+    }
+
+}
